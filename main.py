67adf1734da352bdc324f4cd38c1e46c10430789
mohamedalifaragitiai <mohamed.ali.farag.iti.ai@gmail.com>
2025-10-17 11:05:44 +0300

    adding routes and get values form .env file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to the RAG Chatbot API!"}
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+from routes import base
+app = FastAPI()
+app.include_router(base.base_router)
